7919cdd65e511b3db03cb43417e04c5ddf115a6d
Juan <juan_heguilen@hotmail.com>
2024-07-08 18:59:23 -0300

    first-style

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,15 +7,15 @@ const Footer = () => {
                 <div className='sectionContacts'>
                     <h3>Contactanos</h3>
                     <div>
-                        <img src="" alt="" />
+                        <img src="/icons/instagram.svg" alt="instagramIcon" width={20} height={20} />
                         <span>Instagram</span>
                     </div>
                     <div>
-                        <img src="" alt="" />
+                        <img src="/icons/whatsapp.svg" alt="iconWhatsapp" width={20} height={20} />
                         <span>Whatsap</span>
                     </div>
                     <div>
-                        <img src="" alt="" />
+                    <img src="/icons/facebook.svg" alt="iconFace" width={20} height={20} />
                         <span>Facebook</span>
                     </div>
 
@@ -23,11 +23,11 @@ const Footer = () => {
                 <div className='buy'>
                     <h3>Medios de pagos</h3>
                     <div>
-                        <img src="" alt="" />
+                        <img src="/icons/cash.svg" alt="iconCash" width={20} height={20}/>
                         <span>Efectivo</span>
                     </div>
                     <div>
-                        <img src="" alt="" />
+                        <img src="/icons/mpsvg.svg" alt="iconMp" width={22} height={22} />
                         <span>Transferencia</span>
                     </div>
                 </div>
